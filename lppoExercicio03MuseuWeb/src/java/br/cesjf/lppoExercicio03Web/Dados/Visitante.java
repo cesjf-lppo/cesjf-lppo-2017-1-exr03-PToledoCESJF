f00372a1bc1664da747e6ced6bbc137c91e81fb9
Octavio Silveira <octavio.silveira@hotmail.com>
2017-03-27 22:28:49 -0300

    Criando visitanteSQL Criando Visitante.java Criando ArirConexao.java

---
+++ lppoExercicio03MuseuWeb/src/java/br/cesjf/lppoExercicio03Web/Dados/Visitante.java
@@ -0,0 +1,59 @@
+package br.cesjf.lppoExercicio03Web.Dados;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aluno
+ */
+public class Visitante {
+    private Long id;
+    private String nome;
+    private int idade;
+    private Date entrada;
+    private Date saida;
+
+    public Visitante() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public Date getEntrada() {
+        return entrada;
+    }
+
+    public void setEntrada(Date entrada) {
+        this.entrada = entrada;
+    }
+
+    public Date getSaida() {
+        return saida;
+    }
+
+    public void setSaida(Date saida) {
+        this.saida = saida;
+    }
+
+}
